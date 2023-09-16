426bd019b52fa8e687b85868dda42264abd6651a
yusufva <yusufvalent@gmail.com>
2023-09-16 20:22:55 +0700

    feat(dto): adding NewUserRequest and response dto

--- dto/user.go
+++ dto/user.go
@@ -1,5 +1,16 @@
 package dto
 
+type NewUserRequest struct {
+	Username string `json:"username" valid:"required~username cannot be empty,alphanum"`
+	Password string `json:"password" valid:"required~password cannot be empty,stringlength(6|72)"`
+}
+
+type NewUserResponse struct {
+	Result     string `json:"result"`
+	StatusCode int    `json:"statuscode"`
+	Message    string `json:"message"`
+}
+
 type LoginRequest struct {
 	Username string `json:"username" valid:"required~username tidak boleh kosong"`
 	Password string `json:"password" valid:"required~password tidak boleh kosong"`
